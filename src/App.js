8de206479ca79065479c227f2a7d32f0550ceb35
Jisbel Rodríguez <jisbelcarolina@gmail.com>
2023-08-05 13:57:44 -0400

    se agrego la inplementacion de custom hooks

--- src/App.js
+++ src/App.js
@@ -13,22 +13,33 @@ import { CreateTodoButton } from './CreateTodoButton';
 //   { text: 'Usar estados derivados', completed: true }, 
 // ];
 
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-// localStorage.remoceItem('TODOS_V1');
+// localStorage.setItem(itemName, JSON.stringify(defaultTodos));
+// localStorage.remoceItem(itemName);
 
-function App() {
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
   
-  let parsedTodos;
+  let parsedItem;
 
-  if (!localStorageTodos) {
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
   } else {
-    parsedTodos =  JSON.parse(localStorageTodos);
+    parsedItem =  JSON.parse(localStorageItem);
   }
 
-  const [todos, setTodos] = React.useState(parsedTodos);
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem));  
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
+
+function App() {
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);
   const [searchValue, setsearchValue] = React.useState( '' );
 
   const completedTodos = todos.filter(
@@ -44,11 +55,7 @@ function App() {
     }
   );
 
-  const saveTodos = (newTodos) => {
-    localStorage.setItem('TODOS_V1', JSON.stringify(newTodos));
-    
-    setTodos(newTodos);
-  };
+
 
   const completeTodo = (text) => {
     const newTodos = [...todos];
